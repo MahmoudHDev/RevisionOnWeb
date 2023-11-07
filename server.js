cbc01c6ebc0018f367687588a04ae049a3d1b2ff
Mahmoud Hashim <Mahmoud.ios.dev@gmail.com>
2023-11-07 13:23:45 +0200

    check if the string empty or not

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import path from'path';
+import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-import { dir } from 'console';
-
 
 
 const app = express();
@@ -18,25 +16,39 @@ app.set('view engine', 'ejs');
 app.use('/public', express.static(path.join(__dirname, "public")));
 
 // Home
-app.get('/', (req,res) => {
-    res.render( __dirname + '/views/home.ejs')
-    
+app.get('/', (req, res) => {
+    res.render(__dirname + '/views/home.ejs')
+
 });
 
-app.post('/', (req,res)=> { 
+app.post('/', (req, res) => {
     console.log(req.body);
+    const body = req.body;
+    allfilled(body.usernameText, body.emailText, body.passwordText)
 });
+
+
+var allfilled = function (user, email, pass) { 
+    console.log(user, email, pass);
+    if (user.length === 0) { 
+        console.log("String is empty");
+
+    }else{ 
+        console.log("proceed to next step");
+    }
+    console.log(user.length);
+};
 
 // About
-app.get('/about', (req,res) => {
+app.get('/about', (req, res) => {
     res.render("about")
-    
+
 });
 
-app.get('/projects', (req,res) => {
+app.get('/projects', (req, res) => {
     res.render("projects")
-    
+
 });
-app.listen(port, () => { 
+app.listen(port, () => {
     console.log("server started listening.")
 })
